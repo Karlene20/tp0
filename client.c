01c615bb513329d885e94cad814fffa4cafb4c33
Escobar Karlene <kescobar@frba.utn.edu.ar>
2026-04-15 15:58:25 -0300

    Se termino el tp 0

---
+++ client.c
@@ -0,0 +1,150 @@
+#include "client.h"
+
+int main(void)
+{
+	/*---------------------------------------------------PARTE 2-------------------------------------------------------------*/
+
+	int conexion;
+	char* ip;
+	char* puerto;
+	char* valor;
+
+	t_log* logger;
+	t_config* config;
+
+	/* ---------------- LOGGING ---------------- */
+
+	logger = iniciar_logger();
+
+	// Usando el logger creado previamente
+	// Escribi: "Hola! Soy un log"
+	log_info(logger, "Hola! Soy un log");
+
+
+	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
+
+	config = iniciar_config();
+
+	// Usando el config creado previamente, leemos los valores del config y los 
+	// dejamos en las variables 'ip', 'puerto' y 'valor'
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+	valor = config_get_string_value(config, "CLAVE");
+
+	// Loggeamos el valor de config
+	log_info(logger, "IP: %s", ip);
+	log_info(logger, "PUERTO: %s", puerto);
+	log_info(logger, "CLAVE: %s", valor);
+
+
+	/* ---------------- LEER DE CONSOLA ---------------- */
+
+	leer_consola(logger);
+
+	/*---------------------------------------------------PARTE 3-------------------------------------------------------------*/
+
+	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
+
+	// Creamos una conexión hacia el servidor
+	conexion = crear_conexion(ip, puerto);
+
+	// Enviamos al servidor el valor de CLAVE como mensaje
+	enviar_mensaje(valor,conexion);
+	
+	// Armamos y enviamos el paquete
+	paquete(conexion);
+
+	terminar_programa(conexion, logger, config);
+
+	/*---------------------------------------------------PARTE 5-------------------------------------------------------------*/
+	// Proximamente
+	//para saber si termino el programa
+	printf("Cliente Cerrado\n");	
+}
+
+t_log* iniciar_logger(void)
+{
+	t_log* nuevo_logger;
+	//t_log * 	log_create (char *file, char *process_name, bool is_active_console, t_log_level level)
+	nuevo_logger = log_create("cliente.log", "LOGGER", true, LOG_LEVEL_INFO);	
+	
+	if(nuevo_logger == NULL)
+	{
+		printf("Error no se pudo crear el logger\n");
+		exit(EXIT_FAILURE);
+	}
+	return nuevo_logger;
+}
+
+t_config* iniciar_config(void)
+{
+	t_config* nuevo_config;
+
+	nuevo_config = config_create("cliente.config");
+
+	if (nuevo_config == NULL)
+	{
+		printf("Error no se pudo leer el config\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return nuevo_config;
+}
+
+void leer_consola(t_log* logger)
+{
+	char* leido;
+
+	// La primera te la dejo de yapa
+	leido = readline("> ");
+	log_info(logger, " >> Leí: %s", leido);
+
+	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
+	while(strcmp(leido, "") != 0){
+		free(leido);
+		leido = readline("> ");
+		log_info(logger, " >> Leí: %s", leido);
+	}
+
+	// ¡No te olvides de liberar las lineas antes de regresar!
+	free(leido);
+
+	
+}
+
+void paquete(int conexion)
+{
+	// Ahora toca lo divertido!
+	char* leido;
+	t_paquete* paquete = crear_paquete();
+
+	// Leemos y esta vez agregamos las lineas al paquete
+	leido = readline("> ");
+	//log_info(logger, " >> Leí: %s", leido);
+
+	while(strcmp(leido, "") != 0){
+		agregar_a_paquete(paquete, leido, strlen(leido) + 1); //strlen te va a contar el \0, por eso le sumamos 1
+		free(leido);
+		leido = readline("> ");
+		//log_info(logger, " >> Leí: %s", leido);
+	}
+
+	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
+	free(leido);
+
+	//Enviamos el paquete
+	enviar_paquete(paquete, conexion);
+
+	//Eliminamos el paquete
+	eliminar_paquete(paquete);
+	
+}
+
+void terminar_programa(int conexion, t_log* logger, t_config* config)
+{
+	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
+	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	  log_destroy(logger);
+	  config_destroy(config);
+	  liberar_conexion(conexion);
+}
